82961f55dd8ccb0323617d665879ca89dc7cb977
DeeBest <simphiwedladla8@gmail.com>
2025-04-21 14:28:01 +0200

    created page for adding new product

--- frontend/src/components/AddProduct.jsx
+++ frontend/src/components/AddProduct.jsx
@@ -1,4 +1,97 @@
+import { useState } from 'react';
+
 const AddProduct = () => {
-  return <div>AddProduct</div>;
+  const [productTitle, setProductTitle] = useState('');
+  const [productDesc, setProductDesc] = useState('');
+  const [productPrice, setProductPrice] = useState(0);
+  const [productCategory, setProductCategory] = useState('Uncategorized');
+  const [productImage, setProductImage] = useState('');
+  const [productImagePreview, setProductImagePreview] = useState(null);
+
+  const handleAddProduct = async (e) => {
+    e.preventDefault();
+    const newProduct = {
+      productTitle,
+      productPrice,
+      productDesc,
+      productCategory,
+      productImage,
+    };
+    console.log(newProduct);
+  };
+
+  const handleImageUpload = async (e) => {
+    e.preventDefault();
+    console.log('image upload');
+  };
+
+  return (
+    <div className="w-full flex flex-col gap-3 items-center justify-center ">
+      <h3 className="text-xl font-serif mb-5 ">Add New Product</h3>
+      <form className="flex gap-3 items-center justify-between w-full max-w-[400px] ">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={(e) => {
+            setProductImage(e.target.files[0]);
+            setProductImagePreview(URL.createObjectURL(e.target.files[0]));
+          }}
+        />
+        <button
+          className="bg-amber-600 text-white font-bold rounded-md p-1 hover:opacity-65 duration-300 whitespace-nowrap "
+          onClick={handleImageUpload}
+        >
+          Upload Image
+        </button>
+      </form>
+      {productImagePreview && (
+        <img
+          src={productImagePreview}
+          alt="product image"
+          className="w-[150px] "
+        />
+      )}
+      <form className="w-full flex flex-col gap-2 max-w-[400px] ">
+        <input
+          type="text"
+          placeholder="Name"
+          value={productTitle}
+          onChange={(e) => setProductTitle(e.target.value)}
+          className="w-full p-1 border border-amber-600 rounded-md text-sm "
+        />
+        <input
+          type="number"
+          placeholder="Price"
+          value={productPrice}
+          onChange={(e) => setProductPrice(Number(e.target.value))}
+          className="w-full p-1 border border-amber-600 rounded-md text-sm "
+        />
+        <textarea
+          placeholder="Description"
+          value={productDesc}
+          onChange={(e) => setProductDesc(e.target.value)}
+          className="w-full p-1 border border-amber-600 rounded-md text-sm "
+        />
+        <select
+          value={productCategory}
+          onChange={(e) => setProductCategory(e.target.value)}
+          className="w-full p-1 border border-amber-600 rounded-md text-sm "
+        >
+          <option value="Uncategorized">Select Category</option>
+          <option value="Shoes">Shoes</option>
+          <option value="Suits">Suits</option>
+          <option value="Weaves">Weaves</option>
+          <option value="Combos">Combos</option>
+          <option value="Iphones">Iphones</option>
+        </select>
+        <button
+          onClick={handleAddProduct}
+          className="bg-amber-600 text-white font-bold rounded-md self-center p-1 hover:opacity-65 whitespace-nowrap duration-300 "
+        >
+          Add Product
+        </button>
+      </form>
+    </div>
+  );
 };
 export default AddProduct;
--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -4,13 +4,19 @@ import { FaPlus, FaList } from 'react-icons/fa';
 const AdminPage = () => {
   return (
     <div className="flex w-full gap-4 ">
-      <aside>
-        <Link to="/admin">
-          <FaPlus />
+      <aside className="flex flex-col gap-4 ">
+        <Link
+          to="/admin"
+          className="flex whitespace-nowrap items-center gap-2 text-sm hover:shadow-none hover:opacity-65 duration-300 p-2 rounded-md font-bold shadow-sm shadow-[rgba(0,0,0,0.3)] "
+        >
+          <FaPlus className="text-green-600 " />
           <span>Add Product</span>
         </Link>
-        <Link to="/admin/products-list">
-          <FaList />
+        <Link
+          to="/admin/products-list"
+          className="flex whitespace-nowrap items-center gap-2 text-sm hover:shadow-none hover:opacity-65 duration-300 p-2 rounded-md font-bold shadow-sm shadow-[rgba(0,0,0,0.3)] "
+        >
+          <FaList className="text-amber-600 " />
           <span>Products List</span>
         </Link>
       </aside>
